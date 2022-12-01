f59136f4f0619d7a25cc368329f149409c400f42
Sushant Rahapal <sushantrahapal@gmail.com>
2022-12-01 12:47:45 +0545

    Created Class in models

---
+++ lib/models/details.dart
@@ -0,0 +1,13 @@
+class Details {
+  String id;
+  String name;
+  String address;
+  String phone;
+
+  Details({
+    required this.id,
+    required this.name,
+    required this.address,
+    required this.phone,
+  });
+}
